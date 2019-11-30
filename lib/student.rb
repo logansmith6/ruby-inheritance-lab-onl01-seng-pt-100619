c336ff71dbad9a1efff23b6019f5a38076c52784
Logan Smith <logsmith06@gmail.com>
2019-11-30 18:42:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,4 @@
 class Student < User
 
-  attr_accessor :first, :last
-
-  def initialize(first, last)
-    @first = first
-    @last = last
-  end
-
-  def first_name
-    @first
-  end
-
-  def last_name
-    @last
-  end
-
+  
 end
