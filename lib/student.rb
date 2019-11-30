2996ab0570f7d980d800a57dd2b2cf7d987726b8
Logan Smith <logsmith06@gmail.com>
2019-11-30 18:45:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,13 @@
 class Student < User
 
+  attr_accessor :knowledge
+
+  def initialize(knowledge = [])
+    @knowledge = knowledge
+  end
+
+    def learn(string)
+      @knowledge << string 
+    end
 
 end
